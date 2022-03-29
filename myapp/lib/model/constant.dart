3eecddf45b3241619a36c1d6694ce929bfab1a0b
Omieibi Bagshaw <62633996+Bagshaw000@users.noreply.github.com>
2022-03-29 02:21:02 +0000

    Implemented the database connection and retrival of data from the database

---
+++ myapp/lib/model/constant.dart
@@ -0,0 +1,9 @@
+const MONGO_CONN_URL =
+    "mongodb+srv://Bagshaw:6mBUyPSBIhXVDQa6@matt.plnfh.mongodb.net/General?retryWrites=true&w=majority";
+const ClassPeriod_Coll =
+    "ClassPeriods"; // The collection name we are calling from the db
+const Classrooms_Coll = "Classrooms";
+const Courses_Coll = "Courses";
+const FacultyInterns_Coll = "FacultyInterns";
+const Lecturers_Coll = "Lecturers";
+const Spring22_Coll = "Spring22";
